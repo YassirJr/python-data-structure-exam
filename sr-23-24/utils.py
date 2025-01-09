e804f57806ede475d942fc77956dd30ca9f11d76
YassirJr <yassir.junior.03@gmail.com>
2025-01-09 22:06:51 +0100

    create the utils file

---
+++ sr-23-24/utils.py
@@ -0,0 +1,4 @@
+Employes = ['Alami', 'Bassou', 'Amraoui', 'Baghdadi', 'Mohammadi', 'Alaoui']
+Salaires = [15345.93, 7850.75, 12780.00, 4500.25, 17575.53, 3750.29]
+Services = ['Commercial', 'Comptabilité', 'Approvisionnement', 'Commercial', 'Approvisionnement', 'Comptabilité']
+
